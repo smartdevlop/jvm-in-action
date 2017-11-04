8b0478f2d2c948fc8f7172e845ad2c42e41e8646
abelwmw <14510935@qq.com>
2017-11-04 15:44:48 +0800

    add

---
+++ src/geym/zbase/ch6/hold/HProfTest.java
@@ -0,0 +1,42 @@
+package geym.zbase.ch6.hold;
+/**
+ * -agentlib:hprof=cpu=times,interval=10
+-agentlib:hprof=heap=dump,format=b,file=.\core.hprof
+-agentlib:hprof=heap=sites
+ * @author abel
+ *
+ */
+public class HProfTest {
+	  public void slowMethod()
+	  {
+	    try {
+	      Thread.sleep( 1000 );					//模拟一个很慢的方法
+	    } catch (InterruptedException e) {
+	      e.printStackTrace();     }
+	  }
+
+	  public void slowerMethod()
+	  {
+	    try {
+	      Thread.sleep( 10000 );					//模拟一个更慢的方法
+	    } catch (InterruptedException e) {
+	      e.printStackTrace();     }
+	  }
+
+	  public void fastMethod()						//一个很快的方法
+	  {
+	    try {
+	      Thread.yield();
+	    } catch (Exception e) {
+	      e.printStackTrace();     }
+	  }
+
+	  public static void main( String[] args )
+	  {
+	    HProfTest test = new HProfTest();
+	    test.fastMethod();						//分别运行这些方法
+	    test.slowMethod();
+	    test.slowerMethod();
+	  }
+}
+
